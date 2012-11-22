b7c4e51b2809f12cc545799c08091521d88c2c11
hamaco <hamanaka.kazuhiro@gmail.com>
2012-11-23 00:09:10 +0900

    htmlディレクトリ全体を対象にした

--- sami.php
+++ sami.php
@@ -8,7 +8,7 @@ $iterator = Finder::create()
 	->name("*.php")
 	->exclude("Resources")
 	->exclude("Tests")
-	->in(__DIR__ . "/_momonga/html/modules/xcore")
+	->in(__DIR__ . "/_momonga/html")
 ;
 
 return new Sami($iterator, array(
