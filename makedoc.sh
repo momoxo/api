fc7f9f3298164041ac668aec32dca791e6323113
hamaco <hamanaka.kazuhiro@gmail.com>
2012-11-22 11:16:21 +0900

    Update document.

--- makedoc.sh
+++ makedoc.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
 
-WORKING_DIR=${0%/*}
+## MEMO
+# hub clone -p -b gh-pages momonga-project/api _deploy
+##
 
-echo $WORKING_DIR
+WORKING_DIR=${0%/*}
 
 cd $WORKING_DIR
 
 php composer.phar self-update
-
 if [ ! -d $WORKING_DIR/vendor ]; then
   php composer.phar install
 else
   php composer.phar update
 fi
 
+pushd $WORKING_DIR/_momonga
+git pull --rebase
+popd
+
 ./vendor/bin/sami.php update "${WORKING_DIR}/sami.php"
+
+pushd $WORKING_DIR/_deploy
+git add .
+git commit -a -m 'Update document.'
+git push origin gh-pages
+popd
