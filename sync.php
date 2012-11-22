37b6801ec2677e7cdb5aa44ddd09ddf3a2ac102a
hamaco <hamanaka.kazuhiro@gmail.com>
2012-11-22 12:02:04 +0900

    )が1個足りなかった……

--- sync.php
+++ sync.php
@@ -1,5 +1,7 @@
 <?php
 
-if (in_array($_SERVER['REMOTE_ADDR'], array('207.97.227.253', '50.57.128.197', '108.171.174.178')) {
+if (in_array($_SERVER['REMOTE_ADDR'], array('207.97.227.253', '50.57.128.197', '108.171.174.178'))) {
 	system(__DIR__ . '/makedoc.sh');
+} else {
+	die('WAHAHA');
 }
